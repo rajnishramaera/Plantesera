9bdcde1d555c40450d34d0bc733d4688d1320bbe
rajnishramaera <rajnish.it@ramaera.in>
2023-03-31 15:36:53 +0530

    added redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
 import App from "./App"
 import "./index.css"
+import { store } from "../state/store"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 )
