cfe3eaf8e34878bf7c0043fcfcb3a89ac11d2dfd
rajnishramaera <rajnish.it@ramaera.in>
2023-04-04 16:18:45 +0530

    footer 2023

--- src/Components/Footer/Sidebar.jsx
+++ src/Components/Footer/Sidebar.jsx
@@ -1,4 +1,3 @@
-import React from "react"
 import "./footer.css"
 
 const SideBar = () => {
@@ -8,8 +7,8 @@ const SideBar = () => {
         {/* <div className="lg:basis-9/12 lg:h-[21.5rem] bg-red-50 imaged"></div> */}
 
         <div className="lg:basis-3/12 lg:h-[21.5rem] bg-black flex flex-col justify-start side-bar">
-          {/* <div className="lg:w-40 lg:my-auto ml-10 siderbar-container">
-            <div className="mb-5">
+          <div className="lg:w-40 lg:my-auto ml-10 siderbar-container">
+            {/* <div className="mb-5">
               <p className="text-sm text-white">More Opinions</p>
               <p className="text-xs text-gray-400 font-thin">
                 Amet minim mollit non deserunt ullamco est sit aliqua dolor do
@@ -29,8 +28,13 @@ const SideBar = () => {
                 Amet minim mollit non deserunt ullamco est sit aliqua dolor do
                 amet sint.
               </p>
+            </div> */}
+            <div className="">
+              <p className="text-gray-400 text-xs font-thin">
+                The Copyright 2023 guide covers 14 jurisdictions.
+              </p>
             </div>
-          </div> */}
+          </div>
         </div>
       </div>
     </>
